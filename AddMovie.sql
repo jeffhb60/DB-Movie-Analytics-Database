7b5452c48305acc46017b2803a5b32df7a5a96bf
Jeff Butt <168610176+jeffhb60@users.noreply.github.com>
2024-09-05 00:12:10 -0400

    Create AddMovie.sql

---
+++ AddMovie.sql
@@ -0,0 +1,27 @@
+-- Stored Procedure to Add a New Movie:
+DELIMITER //
+CREATE PROCEDURE AddMovie(
+    IN title VARCHAR(150),
+    IN release_date DATE,
+    IN genre VARCHAR(50),
+    IN language VARCHAR(50),
+    IN duration INT,
+    IN director_id INT
+)
+BEGIN
+    INSERT INTO Movies (title, release_date, genre, language, duration, director_id)
+    VALUES (title, release_date, genre, language, duration, director_id);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE FUNCTION GetAverageRating(movieId INT) RETURNS DECIMAL(3, 2)
+BEGIN
+    DECLARE avgRating DECIMAL(3, 2);
+    SELECT AVG(rating) INTO avgRating
+    FROM Ratings
+    WHERE movie_id = movieId;
+    RETURN avgRating;
+END //
+DELIMITER ;
+
